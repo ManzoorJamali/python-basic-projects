b288918d5d3a5043e5ebce00572093aa001bdd6f
ManzoorJamali <jamalimanzoor05@gmail.com>
2024-12-03 11:14:16 +0500

    Age Checker in python
    simple mini project age checker

---
+++ age_checker.py
@@ -0,0 +1,70 @@
+
+# check your birth  
+# import datetime
+# now = datetime.datetime.now()
+# print('Current date and time:', now)
+
+# birth = input('enter your Birth Year: ')
+# month = input('enter month: ')
+# day = input('enter days: ')
+# d = 30 - int(day)
+# c = 11 - int(month)
+# age  = 2024 - int(birth)
+# print(age)
+# print(f'Your age is  {age} year , {c} Month and {d} days :')
+
+
+#set examples 
+# fruit = {'mango', 'banana','Orange','cherry'}
+
+# # print(dir(fruit))
+# # print(help(fruit))
+# fruit.append('pineapple')
+# fruit.insert(0,'MilkShape')
+# fruit.remove('banana')
+# fruit.reverse()
+# fruit.sort()
+# print(fruit)
+# # for x in fruit:
+# #     print(x)
+
+
+# #list examples 
+# fruit = ['mango', 'banana','Orange','cherry']
+# # print(dir(fruit))
+# # print(help(fruit))
+# fruit.append('pineapple')
+# fruit.insert(0,'MilkShape')
+# fruit.remove('banana')
+# fruit.reverse()
+# fruit.sort()
+# print(fruit)
+# # for x in fruit:
+# #     print(x)
+  
+# print(3 * 3) #output 9
+# print(3 ** 3) # for Power output 27 
+# print(11 / 3)# output  3.6666666666666665
+# print(11 // 3) # output 3
+# x = 10 + 4 * 3 / 2
+# print(x)
+
+# num = [1,2,3,4,5,6]
+# # num.append(7) #apend item add last in index
+# num.insert(3,-4)# insert can add any where in list 
+# print(num)
+
+
+# tuples
+
+person = ("Manzoor",32, 'Karachi')
+name, age, city = (person)
+print(name)
+print(age)
+print(city)
+
+person = (name, age, city)
+name = "ayaz"
+age = 22
+city = 'dadu'
+
